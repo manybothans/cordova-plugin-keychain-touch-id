85acb6b7dda6c99fdf164f5ccb6dbdf3e4abae5b
Jess Latimer <jplatimer@gmail.com>
2017-01-05 09:08:40 -0800

    small change

--- www/touchid.js
+++ www/touchid.js
@@ -1,25 +1,32 @@
 var argscheck = require('cordova/argscheck'),
-               exec = require('cordova/exec');
+	exec = require('cordova/exec');
 
 var touchid = {
-	isAvailable: function(successCallback, errorCallback){
+	isAvailable: function (successCallback, errorCallback) {
 		exec(successCallback, errorCallback, "TouchID", "isAvailable", []);
 	},
-	save: function(key,password, successCallback, errorCallback) {
-		exec(successCallback, errorCallback, "TouchID", "save", [key,password]);
+	save: function (key, password, successCallback, errorCallback) {
+		exec(successCallback, errorCallback, "TouchID", "save", [key, password]);
 	},
-	verify: function(key,message,successCallback, errorCallback){
-		exec(successCallback, errorCallback, "TouchID", "verify", [key,message]);
+	verify: function (key, message, successCallback, errorCallback) {
+		exec(successCallback, errorCallback, "TouchID", "verify", [key, message]);
 	},
-	has: function(key,successCallback, errorCallback){
+	has: function (key, successCallback, errorCallback) {
 		exec(successCallback, errorCallback, "TouchID", "has", [key]);
 	},
-	delete: function(key,successCallback, errorCallback){
+	delete: function (key, successCallback, errorCallback) {
 		exec(successCallback, errorCallback, "TouchID", "delete", [key]);
 	},
-	setLocale: function(locale,successCallback, errorCallback){
+	setLocale: function (locale, successCallback, errorCallback) {
 		exec(successCallback, errorCallback, "TouchID", "setLocale", [locale]);
 	}
 };
+
+
+if (!window.plugins) {
+	window.plugins = {};
+}
+
+window.plugins.touchid = touchid;
 
 module.exports = touchid;
